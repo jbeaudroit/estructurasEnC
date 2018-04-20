09b1f4c820b1c3387704946f07c573a2d8a1ffe8
Your Name <you@example.com>
2018-04-20 17:36:51 -0300

    estructura de alumnos

---
+++ ejemplo structuras/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    int legajo;
+    int nota1;
+    int nota2;
+    float promedio
+    char nombre[50];
+    int estado;
+
+}ealumnos;
+int main()
+{
+
+
+    return 0;
+}
